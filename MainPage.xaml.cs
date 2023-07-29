6d2b11c81291931ad499915c3f9197c7a7d388e1
bhuwancb99 <65246844+bhuwancb99@users.noreply.github.com>
2023-07-29 22:35:43 +0530

    Initial checkin

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace MauiDemoApp;
+
+using MauiDemoApp.Models;
+using MauiDemoApp.Views;
+using System.Diagnostics;
+
+public partial class MainPage : BaseContentPage
+{
+
+    List<StudentModel> studentModels = new List<StudentModel>();
+
+    public MainPage()
+    {
+
+        try
+        {
+            InitializeComponent();
+        }
+        catch (Exception ex)
+        {
+            Debug.Write(ex.ToString());
+        }
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        studentModels = new List<StudentModel>();
+        studentModels.Add(new StudentModel { Id = 1, StudentName = "A" });
+        studentModels.Add(new StudentModel { Id = 2, StudentName = "B" });
+        studentModels.Add(new StudentModel { Id = 3, StudentName = "C" });
+        studentModels.Add(new StudentModel { Id = 4, StudentName = "D" });
+        CVStudentData.ItemsSource = studentModels;
+    }
+
+    private void CVStudentData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        try
+        {
+
+        }
+        catch (Exception)
+        {
+        }
+    }
+}
+
